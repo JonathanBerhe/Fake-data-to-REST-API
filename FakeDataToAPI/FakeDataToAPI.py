26990e44cef0aa6e5ef4aaf70d83168af4cfd743
Jonathan Berhe <jonathan.berhe.work@gmail.com>
2018-09-01 17:19:45 +0200

    mod

--- FakeDataToAPI/FakeDataToAPI.py
+++ FakeDataToAPI/FakeDataToAPI.py
@@ -17,7 +17,7 @@ def http_post(url, data):
 
     return code
 
-def create_pv_fake_data(MAX_NUMBER_FAKE_INFO):  
+def create_maschine_fake_data(MAX_NUMBER_FAKE_INFO):  
 
     faker = Faker()
     
@@ -25,55 +25,52 @@ def create_pv_fake_data(MAX_NUMBER_FAKE_INFO):
 
     for _ in range(MAX_NUMBER_FAKE_INFO):
         json_data = {
-        'codPv': 'AP040' + str(faker.random_int(min=0, max=9) + faker.random_int(min=0, max=9) + faker.random_int(min=0, max=9)),
+        'codPv': 'MASCHINE00' + str(faker.random_int(min=0, max=9) + faker.random_int(min=0, max=9) + faker.random_int(min=0, max=9)),
         'address': str(faker.street_address()),
         'city': faker.city(),
-        'software': 'PPEU04.09B',
-        'hardware': "PPEU SC 2014",
+        'software': 'genericOS',
+        'hardware': "SERVER",
         'ip': str(faker.ipv4_public(network=False, address_class=None))
         }    
         data.append( json.dumps(json_data) )
 
     return data
 
-def create_patch_fake_data(MAX_NUMBER_FAKE_INFO):  
+def create_package_fake_data(MAX_NUMBER_FAKE_INFO):  
 
     faker = Faker()
     
     data = list()
 
     for _ in range(MAX_NUMBER_FAKE_INFO):
-
-
         json_data = {
-        'name': 'PATCH_' + faker.uri_page(),
+        'name': 'PACKAGE_' + faker.uri_page(),
         'description': 'some actions',
-        'path': 'Telemanut',
+        'path': 'root',
         'reboot': faker.boolean(chance_of_getting_true=50)
         }    
         data.append( json.dumps(json_data) )
 
-
     return data
 
 
-def send_fake_data(pv_data, patch_data):
-    pv_api      = 'http://localhost:4000/api/pv'
-    patch_api   = 'http://localhost:4000/api/patch' 
+def send_fake_data(maschine_data, package_data):
+    maschine_api      = 'http://localhost:4000/api/maschine'
+    package_api   = 'http://localhost:4000/api/package' 
 
     # Send data and take the results
-    for pv in pv_data:
-        pv_result = http_post(pv_api, pv)
+    for maschine in maschine_data:
+        maschine_result = http_post(maschine_api, maschine)
 
-    for patch in patch_data:
-        patch_result = http_post(patch_api, patch)
+    for package in package_data:
+        package_result = http_post(package_api, package)
 
     result = [{
-        'code': pv_result,
+        'code': maschine_result,
         'name': 'PV DATA'
         },
         {
-        'code': patch_result,
+        'code': package_result,
         'name': 'PATCH DATA'
         }]
 
@@ -91,28 +88,28 @@ def print_result(confirm=list()):
     
     for i in range(len(confirm)):
         if (confirm[i]['code'] != 200):
-            print("TYPE DATA " + confirm[i]['name'] + " Error to send data to db!" + tabs + "DETAIL: " + str(confirm[i]['code']))
+            print(f"TYPE DATA {confirm[i]['name']} Error to send data to db!{tabs}DETAIL: {str(confirm[i]['code']) }" )
         else:
-            print("TYPE DATA " + confirm[i]['name'] + " Upload data with success!" + tabs + "DETAIL: " + str(confirm[i]['code']))
+            print(f"TYPE DATA {confirm[i]['name']} Upload data with success!{tabs}DETAIL: {str(confirm[i]['code'])}" )
 
 
-def take_input_parameters():
+def input_parameters():
     try:
         # Step 1. number of pv 
-        pv_to_insert = int(input("Inserire il numero dei puntivendita-fake da inviare al db: "))
-        if(pv_to_insert < 500):
-            print("Verranno generati ed inseriti {} finti punti vendita. \n".format(pv_to_insert))
+        maschine_to_insert = int(input("Inserire il numero delle maschine-fake da inviare al db: "))
+        if(maschine_to_insert < 500):
+            print(f"Verranno generati ed inseriti {maschine_to_insert} finti maschine. \n")
         else:
-            pv_to_insert = input("Inserire correttamente il numero(<500) dei pv: \n")
+            maschine_to_insert = input("Inserire correttamente il numero(<500) delle maschine: \n")
     
         # Step 2. number of patches
-        patch_to_insert = int(input("Inserire il numero dei patch-fake da inviare al db: "))
-        if(patch_to_insert < 500):
-            print("Verranno generati ed inseriti {} finte patch. \n".format(patch_to_insert))
+        package_to_insert = int(input("Inserire il numero dei package-fake da inviare al db: "))
+        if(package_to_insert < 500):
+            print(f"Verranno generati ed inseriti {package_to_insert} finti package. \n")
         else:
-            patch_to_insert = input("Inserire correttamente il numero(<500) delle patch: \n")
+            package_to_insert = input("Inserire correttamente il numero(<500) dei package: \n")
 
-        return [pv_to_insert, patch_to_insert]
+        return [maschine_to_insert, package_to_insert]
 
     except Exception as e:
         print("Errore durante l'inserimento dei parametri richiesti ")
@@ -122,14 +119,13 @@ def main():
     start = time.monotonic()
     print("Start FakeDataToAPI.py")
 
-    params = take_input_parameters()
+    params = input_parameters()
     
-    pv_data = create_pv_fake_data(params[0])
-    patch_data = create_patch_fake_data(params[1])
+    maschine_data = create_maschine_fake_data(params[0])
+    package_data = create_package_fake_data(params[1])
     
     # Send data using restful api
-    print_result(confirm=send_fake_data(pv_data, patch_data))
-
+    print_result(confirm=send_fake_data(maschine_data, package_data))
 
     #  Wait some input..
     print_time(start)
